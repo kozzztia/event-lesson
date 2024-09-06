e53715f042ee7f0a1b25b48b9cd16441a007f7ed
kozzztia <kozzztia@gmail.com>
2024-09-06 11:16:11 +0300

    some test with events

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Project event-lesson initialized")
+const grand = document.getElementById('grand');
+const parent = document.getElementById('parent');
+const child = document.getElementById('child');
+
+
+
+document.body.addEventListener('click', e => {
+    console.log(e.target);
+})
+
+// grand.addEventListener('click', e => {
+//     console.log(e.target)
+//     e.stopPropagation();
+// },{capture : true})
+
+// parent.addEventListener('click', e =>{
+//     console.log(e.target);
+//     e.stopPropagation();
+//     }
+// )
+
+// child.addEventListener('click', e => {
+//     e.stopPropagation();
+//     console.log(e.target)
+// }
+// )
